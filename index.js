c8cab9fd35383c9675378e392195336dc3c839c5
Darrell Richards Jr <drichards@valcom.com>
2018-12-11 15:31:13 -0500

    Added help section to CLI

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const minimist = require('minimist')
 
 module.exports = () => {
     const args = minimist(process.argv.slice(2))
-    const cmd = args._[0]
+    let cmd = args._[0] || 'help'
+
+    if (args.help || args.h) {
+        cmd = 'help'
+    }
 
     switch (cmd) {
         case 'send':
         require('./cmds/send')(args)
+        break
+        case 'help':
+        require('./cmds/help')(args)
         break
         default:
         console.error(`"${cmd}" is not a valid command!`)
