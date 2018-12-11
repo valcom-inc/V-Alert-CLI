9738d0c8a326b16bdf4b601a6b348cf72f5c2759
Darrell Richards Jr <drichards@valcom.com>
2018-12-11 13:28:29 -0500

    Start of CLI

---
+++ cmds/send.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+const ora = require('ora')
+
+module.exports = async (args) => {
+    const spinner = ora('Sending Alert').start()
+    spinner.color = 'cyan'
+    const channels = []
+    console.log(args)
+    if (args.c === true) {
+        return spinner.warn(['Looks as if you passed back -c however did not pass back a string'])
+    } else if (args.c === false) {return spinner.warn(['Looks as if you didnt pass back -c which is required'])}
+
+    if (args.c) {
+        channels.push(args.c)
+    }
+
+    if (args.m === true) {
+       return spinner.warn(['Looks as if you passed back -m however did not pass back a string'])
+    } else if (args.m === false) {return spinner.warn(['Looks as if you didnt pass back -m which is required'])}
+
+    if (args.url === true) {
+        return spinner.warn(['Looks as if you passed back --url however did not pass back a string'])
+    } else if (args.url === false) {return spinner.warn(['Looks as if you didnt pass back --url which is required'])}
+
+    if (args.t === true) {
+        return spinner.warn(['Looks as if you passed back -t however did not pass back a string'])
+    } else if (args.t === false) {return spinner.warn(['Looks as if you didnt pass back -t which is required'])}
+
+
+    const config = { headers: { Authorization: `Basic ${args.t}` } };
+    axios.post(`${args.url}`, {
+        channels,
+        message: args.m
+      }, config)
+      .then(function (response) {
+        spinner.succeed([response.data])
+      })
+      .catch(function (error) {
+        spinner.fail([error])
+      });
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+const minimist = require('minimist')
+
+module.exports = () => {
+    const args = minimist(process.argv.slice(2))
+    const cmd = args._[0]
+
+    switch (cmd) {
+        case 'send':
+        require('./cmds/send')(args)
+        break
+        default:
+        console.error(`"${cmd}" is not a valid command!`)
+        break
+    }
+}
